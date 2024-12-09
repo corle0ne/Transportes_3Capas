adeecda88e1e4ec7b463d7ce83ae06d1038f8a44
corle0ne <melladosolisd@gmail.com>
2024-12-09 12:52:44 -0600

    Se configuro cadena de conexion

---
+++ DataAccessLayer/configuracion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class configuracion
+    {
+        //cadena de conexion
+        //data source = nombre del servidor de BD
+        //localhost
+        //.
+        //Nombre de mi instancia
+        //Initial Catalog = nombre de la DB
+        //Integrated Security = true (Credenciales de la maquina)
+        //=false (Credenciales de accesso)
+        //Se habilitan los campos de
+        //User=''
+        //Password=;
+
+        static string _cadenaConexion = @"Data source= DESKTOP-HGPQJRO\SQLEXPRESS ;
+                                          Initial Catalog = Transportes;
+                                          Integrated Security = true;
+                                           ";
+
+        //Encapsulamiento
+
+        public static string CadenaConexion
+        {
+            get
+            {
+                return _cadenaConexion;
+            }
+        }
+    }
+}
