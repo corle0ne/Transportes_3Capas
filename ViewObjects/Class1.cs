b31e86a8bee2e27495557339bf8be1d620479e12
corle0ne <melladosolisd@gmail.com>
2024-12-09 12:43:02 -0600

    Estructura del Proyecto (Biblioteca de clases)

---
+++ ViewObjects/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewObjects
+{
+    public class Class1
+    {
+    }
+}
